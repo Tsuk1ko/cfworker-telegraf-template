deada2da5faf74bca8850bebbf3c92bec54a6f2a
神代綺凛 <i@loli.best>
2022-08-31 21:56:40 +0800

    fix: spelling errors

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A template for Telegram Bot using [telegraf](https://github.com/telegraf/telegraf) framework on Cloudflare Workers.
 
-See more on [cfworker-middware-telegraf](https://github.com/Tsuk1ko/cfworker-middware-telegraf).
+See more on [cfworker-middleware-telegraf](https://github.com/Tsuk1ko/cfworker-middleware-telegraf).
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { Telegraf } = require('telegraf');
 const { Application, Router } = require('@cfworker/web');
-const createTelegrafMiddware = require('cfworker-middware-telegraf');
+const createTelegrafMiddleware = require('cfworker-middleware-telegraf');
 
-const bot = new Telegraf('BOT_TOKEN');
+const bot = new Telegraf(self.BOT_TOKEN);
 
 // Your code here, but do not `bot.launch()`
+// Do not forget to set environment variables BOT_TOKEN and SECRET_PATH on your worker
 
 const router = new Router();
-router.post('/SECRET_PATH', createTelegrafMiddware(bot));
+router.post(`/${self.SECRET_PATH}`, createTelegrafMiddleware(bot));
 new Application().use(router.middleware).listen();
